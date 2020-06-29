4044b11c95f69c44911afba892796a2a8cd16722
viethung512 <ngohung05121997.dev@gmail.com>
2020-06-29 21:33:40 +0700

    Updates

---
+++ precache-manifest.0cdaca75d61e662d9c6661a1ad135b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b89720a26a64270442ab9bebd7652d42",
+    "url": "/MyTravelFrontend/index.html"
+  },
+  {
+    "revision": "26287ca75f6b0680adaa",
+    "url": "/MyTravelFrontend/static/css/2.6b7afa49.chunk.css"
+  },
+  {
+    "revision": "7e67be2bf18aa125a7c7",
+    "url": "/MyTravelFrontend/static/css/main.fbc179dd.chunk.css"
+  },
+  {
+    "revision": "26287ca75f6b0680adaa",
+    "url": "/MyTravelFrontend/static/js/2.beb3f5e8.chunk.js"
+  },
+  {
+    "revision": "7e67be2bf18aa125a7c7",
+    "url": "/MyTravelFrontend/static/js/main.f0633555.chunk.js"
+  },
+  {
+    "revision": "746cc5f26f7f10aa7897",
+    "url": "/MyTravelFrontend/static/js/runtime~main.7aaea2c1.js"
+  },
+  {
+    "revision": "ce32441097e469dc3db208000d1b56e8",
+    "url": "/MyTravelFrontend/static/media/hue.ce324410.jpg"
+  }
+]);
