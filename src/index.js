56836e029b6cf962bbe31c783ceac49b32d6e2c5
HuyenHaru <34090690+HuyenHaru@users.noreply.github.com>
2020-07-06 23:23:24 +0700

    commit code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import './index.css';
-import './../node_modules/video-react/dist/video-react.css'; // import css
-import App from './app/Layout/App';
-import * as serviceWorker from './serviceWorker';
-import store from './app/redux/store';
-import 'antd/dist/antd.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import "./index.css";
+import "./../node_modules/video-react/dist/video-react.css"; // import css
+import App from "./app/Layout/App";
+import * as serviceWorker from "./serviceWorker";
+import store from "./app/redux/store";
+// import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 // serviceWorker();
 serviceWorker.unregister();
